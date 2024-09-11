3918882ab8f455e69dd8c8f83758f0d478f6de43
Phattaradanai Sornsawang <phattaradanai.sor@gmail.com>
2024-09-11 10:16:48 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,3 +31,21 @@ with st.sidebar:
   bmi = st.slider('BMI',0,50,25)
   HbA1c_level = st.slider('HbA1c Level',0,10,5)
   blood_glucose_level = st.slider('Blood Glucose Level',50,400,100)
+
+  new_data = {'gender':gender,
+              'age':age,
+              'hypertension':hypertension,
+              'heart_disease':heart_disease,
+              'smoking_history':smoking_history,
+              'bmi':bmi,
+              'HbA1c_level':HbA1c_level,
+              'blood_glucose_level':blood_glucose_level}
+  
+  input_df = pd.Dataframe(new_data,index=[0])
+  merge_df = pd.concat([input_df,X_raw],axis=0)
+  
+          
+              
+with st.expander('Input Features'):
+  st.write('**Your input**')
+  input_df
