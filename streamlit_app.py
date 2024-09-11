58068ef6e01202aadd73a89ec438c8c12fcab20b
Phattaradanai Sornsawang <phattaradanai.sor@gmail.com>
2024-09-11 10:20:02 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,9 +28,9 @@ with st.sidebar:
   hypertension = st.selectbox('Hypertension',{'Yes','No'})
   heart_disease = st.selectbox('Heart Disease',{'Yes','No'})
   smoking_history = st.selectbox('Smoking History',{'Never','Current','Former'})
-  bmi = st.slider('BMI',0,50,25)
-  HbA1c_level = st.slider('HbA1c Level',0,10,5)
-  blood_glucose_level = st.slider('Blood Glucose Level',50,400,100)
+  bmi = st.slider('BMI',0.0,50.0,25.0)
+  HbA1c_level = st.slider('HbA1c Level',0.0,10.0,5.0)
+  blood_glucose_level = st.slider('Blood Glucose Level',50.0,400.0,100.0)
 
   new_data = {'gender':gender,
               'age':age,
