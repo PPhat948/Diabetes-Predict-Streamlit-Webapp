45eba0328e3085f5e44c1ffb9cc29b8a57179359
Phattaradanai Sornsawang <phattaradanai.sor@gmail.com>
2024-09-11 09:35:41 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,3 +9,11 @@ with st.expander('Dataset'):
   st.write('This Dataset from : https://www.kaggle.com/datasets/iammustafatz/diabetes-prediction-dataset')
   df = pd.read_csv('diabetes_prediction_dataset.csv')
   df
+
+  st.write('**X**')
+  X_raw = df.drop('diabetes',axis=1)
+  X_raw
+
+  st.write('**y**')
+  y_raw = df['diabetes']
+  y_raw
