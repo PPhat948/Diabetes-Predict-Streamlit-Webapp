2444d0bb95eaabedd6285c01cd03a7a8d7a365db
Phattaradanai Sornsawang <phattaradanai.sor@gmail.com>
2024-09-11 09:31:40 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,5 +5,7 @@ st.title('Machine Learning App: Diabetes Predict')
 st.info('Hello')
 
 with st.expander('Dataset'):
-  st.write('**Raw Data*')
+  st.write('**Raw Data**')
   st.write('This Dataset from : https://www.kaggle.com/datasets/iammustafatz/diabetes-prediction-dataset')
+  df = pd.read_csv('diabetes_prediction_dataset.csv')
+  df
