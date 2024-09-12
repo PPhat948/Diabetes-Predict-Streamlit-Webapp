cf355684853c271ec53d86e91c5bddcd42c5bc89
Phattaradanai Sornsawang <phattaradanai.sor@gmail.com>
2024-09-12 15:30:15 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -105,15 +105,10 @@ with st.expander('Model'):
     score = model.score(X_train,y_train)
     st.write('Model Score:', score)
   if st.button('Predict',type='primary'):
-    progress_text = "Operation in progress. Please wait."
-    my_bar = st.progress(0, text=progress_text)
-
-    for percent_complete in range(100):
-      time.sleep(0.01)
-      my_bar.progress(percent_complete + 1, text=progress_text)
-      time.sleep(1)
-      my_bar.empty()
     model()
+    with st.spinner('Wait for it...'):
+    time.sleep(5)
+    st.success("Done!")
     #st.write('Confusion Matrix')
     #confs = confusion_matrix(y_test,predictions)
     #st.dataframe(confs)
