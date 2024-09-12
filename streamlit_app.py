6119acd7e404d5258b34b7b28cfe4326e2a1dd7e
Phattaradanai Sornsawang <phattaradanai.sor@gmail.com>
2024-09-12 15:56:06 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -106,7 +106,9 @@ def main():
             X_train, X_test, y_train, y_test, input_data = split_data(df_cleaned, df)
             
             # Train the model and get predictions
+            with st.spinner('Wait for it...'):
             predictions_input, model_score = train_and_predict(X_train, y_train, X_test, input_data, model_choice)
+            st.success("Done!")
             
             # Display prediction result
             display_results(predictions_input)
